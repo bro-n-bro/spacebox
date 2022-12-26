fad79f7b65a33f95a2b3bc022b7ce82d525c4354
Victor Neznaykin <va.neznaykin@gmail.com>
2022-12-26 18:42:56 +0400

    consensus_pubkey fixed

--- broker/model/validator.go
+++ broker/model/validator.go
@@ -2,5 +2,5 @@ package model
 
 type Validator struct {
 	ConsensusAddress string `json:"consensus_address"`
-	ConsensusPubKey  string `json:"consensus_pubkey"`
+	ConsensusPubkey  string `json:"consensus_pubkey"`
 }
