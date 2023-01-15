82694cecbda6dc4ab4cc4c55604ccbca1c602950
Victor Neznaykin <va.neznaykin@gmail.com>
2023-01-15 18:52:37 +0400

    fix message model

--- broker/model/message.go
+++ broker/model/message.go
@@ -6,23 +6,5 @@ type Message struct {
 	Signer                    string   `json:"signer"`
 	InvolvedAccountsAddresses []string `json:"involved_accounts_addresses"`
 	Value                     []byte   `json:"value"`
-	MsgIndex                  int64    `json:"msg_index"`
 	Index                     int64    `json:"index"`
-}
-
-func NewMessage(
-	transactionHash, msgType, signer string,
-	index, msgIndex int64,
-	value []byte,
-	involvedAccountsAddresses []string,
-) Message {
-	return Message{
-		TransactionHash:           transactionHash,
-		Index:                     index,
-		Type:                      msgType,
-		Value:                     value,
-		InvolvedAccountsAddresses: involvedAccountsAddresses,
-		Signer:                    signer,
-		MsgIndex:                  msgIndex,
-	}
 }
