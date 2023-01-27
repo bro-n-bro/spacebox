6a3855d0929da08bb759423da1c33e3e3b887625
Victor Neznaykin <va.neznaykin@gmail.com>
2023-01-27 20:32:35 +0400

    model changed

--- broker/model/validator_description.go
+++ broker/model/validator_description.go
@@ -1,17 +1,14 @@
 package model
 
 type (
-	VDescription struct {
+	ValidatorDescription struct {
+		OperatorAddress string `json:"operator_address"`
 		Moniker         string `json:"moniker"`
 		Identity        string `json:"identity"`
 		Website         string `json:"website"`
 		SecurityContact string `json:"security_contact"`
 		Details         string `json:"details"`
-	}
-	ValidatorDescription struct {
-		OperatorAddress string
-		Description     VDescription
-		AvatarURL       string
-		Height          int64
+		AvatarURL       string `json:"avatar_url"`
+		Height          int64  `json:"height"`
 	}
 )
