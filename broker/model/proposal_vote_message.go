07344dd57a121f0e82ff8f30a740a2202696dbdf
Victor Neznaykin <va.neznaykin@gmail.com>
2023-01-30 22:44:43 +0400

    add height to validator model add tx hash to proposal_vote_message

--- broker/model/proposal_vote_message.go
+++ broker/model/proposal_vote_message.go
@@ -4,6 +4,7 @@ type ProposalVoteMessage struct {
 	ProposalID   uint64 `json:"proposal_id"`
 	VoterAddress string `json:"voter"`
 	Option       string `json:"option"`
+	TxHash       string `json:"tx_hash"`
 	Height       int64  `json:"height"`
 	MsgIndex     int64  `json:"msg_index"`
 }
--- broker/model/validator.go
+++ broker/model/validator.go
@@ -4,4 +4,5 @@ type Validator struct {
 	ConsensusAddress string `json:"consensus_address"`
 	OperatorAddress  string `json:"operator_address"`
 	ConsensusPubkey  string `json:"consensus_pubkey"`
+	Height           int64  `json:"height"`
 }
