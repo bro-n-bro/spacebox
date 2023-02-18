2c82c246490f5efae400a0ca711c89a6ed5b8d5b
Victor Neznaykin <va.neznaykin@gmail.com>
2023-02-18 19:16:41 +0400

    block model tx_num field renamed to num_txs

--- broker/model/block.go
+++ broker/model/block.go
@@ -5,7 +5,7 @@ import "time"
 type Block struct {
 	Height          int64     `json:"height"`
 	Hash            string    `json:"hash"`
-	TxNum           int64     `json:"tx_num"`
+	NumTxs          int64     `json:"num_txs"`
 	TotalGas        uint64    `json:"total_gas"`
 	ProposerAddress string    `json:"proposer_address"`
 	Timestamp       time.Time `json:"timestamp"`
