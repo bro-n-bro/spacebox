00ce642beb9ac4ce521ed4f10cd629830fff79c4
MIhas Beliaev <mihasgo@bwg-io.site>
2023-04-08 14:16:55 +0300

    Add msg submit proposal table

--- broker/model/submit_proposal_message.go
+++ broker/model/submit_proposal_message.go
@@ -2,11 +2,11 @@ package model
 
 type SubmitProposalMessage struct {
 	Height         int    `json:"height"`
-	TxHash         string `json:"txHash"`
-	MsgIndex       int    `json:"msgIndex"`
+	TxHash         string `json:"tx_hash"`
+	MsgIndex       int    `json:"msg_index"`
 	Proposer       string `json:"proposer"`
 	Messages       string `json:"messages"`
-	InitialDeposit int    `json:"initialDeposit"`
+	InitialDeposit int    `json:"initial_deposit"`
 	Metadata       string `json:"metadata"`
 	Title          string `json:"title"`
 	Summary        string `json:"summary"`
