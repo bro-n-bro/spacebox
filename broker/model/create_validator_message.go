21db16dbc4c109ec412208ec6e64f29f6691b967
Mihas Generalis <kasinaki97@gmail.com>
2023-04-15 16:00:51 +0300

    add create validator msg

---
+++ broker/model/create_validator_message.go
@@ -0,0 +1,21 @@
+package model
+
+type CreateValidatorMessage struct {
+	Height            int64       `json:"height"`
+	TxHash            string      `json:"tx_hash"`
+	MsgIndex          int64       `json:"msg_index"`
+	DelegatorAddress  string      `json:"delegator_address"`
+	ValidatorAddress  string      `json:"validator_address"`
+	Description       Description `json:"description"`
+	CommissionRates   float64     `json:"commission_rates"`
+	MinSelfDelegation int64       `json:"min_self_delegation"`
+	Pubkey            string      `json:"pubkey"`
+}
+
+type Description struct {
+	Moniker         string `json:"moniker,omitempty"`
+	Identity        string `json:"identity,omitempty"`
+	Website         string `json:"website,omitempty"`
+	SecurityContact string `json:"security_contact,omitempty"`
+	Details         string `json:"details,omitempty"`
+}
