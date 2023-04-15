b80df766368d615bc97011900182b287382d4332
Mihas Generalis <kasinaki97@gmail.com>
2023-04-15 13:04:37 +0300

    change fields

--- broker/model/submit_proposal_message.go
+++ broker/model/submit_proposal_message.go
@@ -1,14 +1,13 @@
 package model
 
 type SubmitProposalMessage struct {
-	Height         int64    `json:"height"`
-	TxHash         string   `json:"tx_hash"`
-	MsgIndex       int64    `json:"msg_index"`
-	Proposer       string   `json:"proposer"`
-	Messages       [][]byte `json:"messages"`
-	InitialDeposit Coins    `json:"initial_deposit"`
-	Title          string   `json:"title"`
-	Description    string   `json:"description"`
-	Type           string   `json:"type"`
-	ProposalID     uint64   `json:"proposal_id"`
+	Height         int64  `json:"height"`
+	TxHash         string `json:"tx_hash"`
+	MsgIndex       int64  `json:"msg_index"`
+	Proposer       string `json:"proposer"`
+	InitialDeposit Coins  `json:"initial_deposit"`
+	Title          string `json:"title"`
+	Description    string `json:"description"`
+	Type           string `json:"type"`
+	ProposalID     uint64 `json:"proposal_id"`
 }
