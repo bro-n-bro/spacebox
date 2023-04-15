840902192e408d95d07d20bb6633c00d9a23f0f8
Mihas Generalis <kasinaki97@gmail.com>
2023-04-15 19:49:28 +0300

    change name

--- broker/model/create_validator_message.go
+++ broker/model/create_validator_message.go
@@ -1,18 +1,18 @@
 package model
 
 type CreateValidatorMessage struct {
-	Height            int64       `json:"height"`
-	TxHash            string      `json:"tx_hash"`
-	MsgIndex          int64       `json:"msg_index"`
-	DelegatorAddress  string      `json:"delegator_address"`
-	ValidatorAddress  string      `json:"validator_address"`
-	Description       Description `json:"description"`
-	CommissionRates   float64     `json:"commission_rates"`
-	MinSelfDelegation int64       `json:"min_self_delegation"`
-	Pubkey            string      `json:"pubkey"`
+	Height            int64                             `json:"height"`
+	TxHash            string                            `json:"tx_hash"`
+	MsgIndex          int64                             `json:"msg_index"`
+	DelegatorAddress  string                            `json:"delegator_address"`
+	ValidatorAddress  string                            `json:"validator_address"`
+	Description       CreateValidatorMessageDescription `json:"description"`
+	CommissionRates   float64                           `json:"commission_rates"`
+	MinSelfDelegation int64                             `json:"min_self_delegation"`
+	Pubkey            string                            `json:"pubkey"`
 }
 
-type Description struct {
+type CreateValidatorMessageDescription struct {
 	Moniker         string `json:"moniker,omitempty"`
 	Identity        string `json:"identity,omitempty"`
 	Website         string `json:"website,omitempty"`
