123031fd827878a1ad8d60c0064be2011ae832f6
Victor Neznaykin <va.neznaykin@gmail.com>
2023-04-18 22:23:17 +0400

    add expiration to GrantAllowanceMessage

--- broker/model/grant_allowance_message.go
+++ broker/model/grant_allowance_message.go
@@ -1,10 +1,13 @@
 package model
 
+import "time"
+
 type GrantAllowanceMessage struct {
-	Height    int64  `json:"height"`
-	MsgIndex  int64  `json:"msg_index"`
-	TxHash    string `json:"tx_hash"`
-	Granter   string `json:"granter"`
-	Grantee   string `json:"grantee"`
-	Allowance []byte `json:"allowance"`
+	Height     int64     `json:"height"`
+	MsgIndex   int64     `json:"msg_index"`
+	TxHash     string    `json:"tx_hash"`
+	Granter    string    `json:"granter"`
+	Grantee    string    `json:"grantee"`
+	Expiration time.Time `json:"expiration"`
+	Allowance  []byte    `json:"allowance"`
 }
