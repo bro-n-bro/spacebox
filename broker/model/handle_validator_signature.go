cbf798ab0f241df120a5844ffa4acdcc49c3892b
Victor Neznaykin <va.neznaykin@gmail.com>
2023-05-11 23:52:08 +0400

    refactor HandleValidatorSignature model

--- broker/model/handle_validator_signature.go
+++ broker/model/handle_validator_signature.go
@@ -1,11 +1,10 @@
 package model
 
 type HandleValidatorSignature struct {
-	Address      string `json:"address"`
-	Power        string `json:"power"`
-	Reason       string `json:"reason"`
-	Jailed       bool   `json:"jailed"`
-	Burned       Coin   `json:"burned"`
-	MissedBlocks int64  `json:"missed_blocks"`
-	Height       int64  `json:"height"`
+	Address string `json:"address"`
+	Power   string `json:"power"`
+	Reason  string `json:"reason"`
+	Jailed  string `json:"jailed"`
+	Burned  Coin   `json:"burned"`
+	Height  int64  `json:"height"`
 }
