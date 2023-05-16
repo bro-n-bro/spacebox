d1dce949e76e5ed7e42fbe78381abe5df75a0489
Victor Neznaykin <va.neznaykin@gmail.com>
2023-05-16 22:05:37 +0400

    use amount as string in DistributionCommission model

--- broker/model/distribution_commission.go
+++ broker/model/distribution_commission.go
@@ -3,5 +3,5 @@ package model
 type DistributionCommission struct {
 	Height    int64  `json:"height"`
 	Validator string `json:"validator"`
-	Amount    Coin   `json:"amount"`
+	Amount    string `json:"amount"`
 }
