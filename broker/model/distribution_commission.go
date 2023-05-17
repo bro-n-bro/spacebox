84dc73975d041e498015a16534406b0bdef4dd4d
Victor Neznaykin <va.neznaykin@gmail.com>
2023-05-17 20:17:56 +0400

    Revert "use amount as string in DistributionCommission model"
    This reverts commit d1dce949e76e5ed7e42fbe78381abe5df75a0489.

--- broker/model/distribution_commission.go
+++ broker/model/distribution_commission.go
@@ -3,5 +3,5 @@ package model
 type DistributionCommission struct {
 	Height    int64  `json:"height"`
 	Validator string `json:"validator"`
-	Amount    string `json:"amount"`
+	Amount    Coin   `json:"amount"`
 }
