0217748a92436ca73a215fa4b7c420cc924f5cab
Victor Neznaykin <va.neznaykin@gmail.com>
2023-05-17 20:23:33 +0400

    fix name of AnnualProvision model

--- broker/model/annual_provision.go
+++ broker/model/annual_provision.go
@@ -1,9 +1,9 @@
 package model
 
 type AnnualProvision struct {
-	Height          int64   `json:"height"`
-	Amount          int64   `json:"amount"`
-	AnnualProvision float64 `json:"annual_provision"`
-	BondedRatio     float64 `json:"bonded_ratio"`
-	Inflation       float64 `json:"inflation"`
+	Height           int64   `json:"height"`
+	Amount           int64   `json:"amount"`
+	AnnualProvisions float64 `json:"annual_provisions"`
+	BondedRatio      float64 `json:"bonded_ratio"`
+	Inflation        float64 `json:"inflation"`
 }
