0039421db7983a45dd0fb63232b6b1aa24dd0c6f
Victor Neznaykin <va.neznaykin@gmail.com>
2023-07-21 17:48:09 +0400

    fix json tags

--- broker/model/multi_send_message.go
+++ broker/model/multi_send_message.go
@@ -3,7 +3,7 @@ package model
 type MultiSendMessage struct {
 	Coins       Coins    `json:"coins"`
 	AddressFrom string   `json:"address_from"`
-	AddressesTo []string `json:"address_to"`
+	AddressesTo []string `json:"addresses_to"`
 	TxHash      string   `json:"tx_hash"`
 	Height      int64    `json:"height"`
 	MsgIndex    int64    `json:"msg_index"`
--- broker/model/proposer_reward.go
+++ broker/model/proposer_reward.go
@@ -1,7 +1,7 @@
 package model
 
 type ProposerReward struct {
-	Height    int64
-	Validator string
-	Reward    Coin
+	Height    int64  `json:"height"`
+	Validator string `json:"validator"`
+	Reward    Coin   `json:"reward"`
 }
