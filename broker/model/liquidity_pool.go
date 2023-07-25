d3f03d2ac1be7423fb7804eed9fb04327f00bfa3
Victor Neznaykin <va.neznaykin@gmail.com>
2023-07-25 11:53:45 +0400

    add liquidity pool

---
+++ broker/model/liquidity_pool.go
@@ -0,0 +1,11 @@
+package model
+
+type LiquidityPool struct {
+	PoolID                uint64 `json:"pool_id"`
+	ReserveAccountAddress string `json:"reserve_account_address"`
+	ADenom                string `json:"a_denom"`
+	BDenom                string `json:"b_denom"`
+	PoolCoinDenom         string `json:"pool_coin_denom"`
+	LiquidityA            Coin   `json:"liquidity_a"`
+	LiquidityB            Coin   `json:"liquidity_b"`
+}
