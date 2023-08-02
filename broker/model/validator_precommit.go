28c11f68596d78622f87632c413c84afd97f1c32
Victor Neznaykin <va.neznaykin@gmail.com>
2023-08-02 11:42:10 +0400

    add validator_precommit model

---
+++ broker/model/validator_precommit.go
@@ -0,0 +1,10 @@
+package model
+
+import "time"
+
+type ValidatorPrecommit struct {
+	Height           int64     `json:"height"`
+	ValidatorAddress string    `json:"validator_address"`
+	BlockIDFlag      uint64    `json:"block_id_flag"`
+	Timestamp        time.Time `json:"timestamp"`
+}
