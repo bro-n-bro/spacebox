5b96005b5b1591c49dc5906b44ca590ab7e9d66f
Victor Neznaykin <va.neznaykin@gmail.com>
2023-08-14 21:15:31 +0400

    add VotingPower field to validator_precommit

--- broker/model/validator_precommit.go
+++ broker/model/validator_precommit.go
@@ -5,6 +5,7 @@ import "time"
 type ValidatorPrecommit struct {
 	Height           int64     `json:"height"`
 	ValidatorAddress string    `json:"validator_address"`
+	VotingPower      int64     `json:"voting_power"`
 	BlockIDFlag      uint64    `json:"block_id_flag"`
 	Timestamp        time.Time `json:"timestamp"`
 }
