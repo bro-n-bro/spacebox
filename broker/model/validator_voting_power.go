c73469d692fd8ddb6855dcb4088bf81c9cf49d4d
Victor Neznaykin <va.neznaykin@gmail.com>
2023-08-16 11:03:57 +0400

    - add validator_voting_power struct - remove voting_power from validator_precommit

--- broker/model/validator_voting_power.go
+++ broker/model/validator_voting_power.go
@@ -2,9 +2,9 @@ package model
 
 import "time"
 
-type ValidatorPrecommit struct {
+type ValidatorVotingPower struct {
 	Height           int64     `json:"height"`
+	VotingPower      int64     `json:"voting_power"`
 	ValidatorAddress string    `json:"validator_address"`
-	BlockIDFlag      uint64    `json:"block_id_flag"`
 	Timestamp        time.Time `json:"timestamp"`
 }
