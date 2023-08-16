a6a7a4e94f72b0d3d3aa7381c76f7e2cbfd372a9
Victor Neznaykin <va.neznaykin@gmail.com>
2023-08-16 20:01:37 +0400

    add proposer_priority and delete timestamp from voting_power

--- broker/model/validator_voting_power.go
+++ broker/model/validator_voting_power.go
@@ -1,10 +1,8 @@
 package model
 
-import "time"
-
 type ValidatorVotingPower struct {
-	Height           int64     `json:"height"`
-	VotingPower      int64     `json:"voting_power"`
-	ValidatorAddress string    `json:"validator_address"`
-	Timestamp        time.Time `json:"timestamp"`
+	Height           int64  `json:"height"`
+	VotingPower      int64  `json:"voting_power"`
+	ValidatorAddress string `json:"validator_address"`
+	ProposerPriority int64  `json:"proposer_priority"`
 }
