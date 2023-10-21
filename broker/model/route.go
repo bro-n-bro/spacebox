3241e099bec5dbd13b0fdab18003d30054389c1c
Reuk Aleksey Sergeevich (reuk.aleksey) <4c1d4u@gmail.com>
2023-10-21 22:32:28 +0400

    add route model

--- broker/model/route.go
+++ broker/model/route.go
@@ -1,6 +1,17 @@
 package model
 
 type (
+	Route struct {
+		Source      string `json:"source"`      //
+		Destination string `json:"destination"` //
+		Alias       string `json:"alias"`       //
+		Timestamp   string `json:"timestamp"`   //
+		TxHash      string `json:"tx_hash"`     //
+		Value       Coin   `json:"value"`       //
+		Height      int64  `json:"height"`      //
+		IsActive    bool   `json:"is_active"`   //
+	}
+
 	CreateRouteMessage struct {
 		Source      string `json:"source"`      //
 		Destination string `json:"destination"` //
