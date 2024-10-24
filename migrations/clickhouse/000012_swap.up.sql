3a2b269661f6eeeffcef1a334472f32ff97c83f1
iljagrabar14 <iljagrabar14@gmail.com>
2024-10-24 23:53:14 +0200

    Add new swap writer without decoder for new blocks

--- migrations/clickhouse/000012_swap.up.sql
+++ migrations/clickhouse/000012_swap.up.sql
@@ -174,7 +174,158 @@ FROM
     (
         SELECT *
         FROM spacebox.raw_block_results
-        WHERE end_block_events ILIKE '%swap_transacted%'
+        WHERE end_block_events ILIKE '%swap_transacted%' and height < 15515000
+    )
+    WHERE JSONExtractString(event,
+ 'type') = 'swap_transacted'
+);
+
+-- spacebox.swap_writer for new blocks source
+
+
+CREATE MATERIALIZED VIEW spacebox.swap_new_blocks_writer TO spacebox.swap
+(
+
+    `height` Int64,
+
+    `timestamp` DateTime,
+
+    `pool_id` String,
+
+    `batch_index` String,
+
+    `swap_requester` String,
+
+    `offer_coin_denom` String,
+
+    `offer_coin_amount` String,
+
+    `demand_coin_denom` String,
+
+    `exchanged_demand_coin_amount` String,
+
+    `transacted_coin_amount` String,
+
+    `remaining_offer_coin_amount` String,
+
+    `offer_coin_fee_amount` String,
+
+    `order_expiry_height` String,
+
+    `exchanged_coin_fee_amount` String,
+
+    `order_price` String,
+
+    `swap_price` String,
+
+    `success` Bool
+)
+AS SELECT
+    height,
+    timestamp,
+    pool_id,
+    batch_index,
+    swap_requester,
+    offer_coin_denom,
+    offer_coin_amount,
+    demand_coin_denom,
+    exchanged_demand_coin_amount,
+    transacted_coin_amount,
+    remaining_offer_coin_amount,
+    offer_coin_fee_amount,
+    order_expiry_height,
+    exchanged_coin_fee_amount,
+    order_price,
+    swap_price,
+    success
+FROM
+(
+    SELECT
+        height,
+        timestamp,
+        arrayJoin(JSONExtractArrayRaw(end_block_events)) AS event,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'pool_id'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS pool_id,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'batch_index'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS batch_index,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'swap_requester'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS swap_requester,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'offer_coin_denom'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS offer_coin_denom,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'offer_coin_amount'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS offer_coin_amount,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'demand_coin_denom'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS demand_coin_denom,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'exchanged_demand_coin_amount'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS exchanged_demand_coin_amount,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'transacted_coin_amount'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS transacted_coin_amount,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'remaining_offer_coin_amount'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS remaining_offer_coin_amount,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'offer_coin_fee_amount'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS offer_coin_fee_amount,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'order_expiry_height'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS order_expiry_height,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'exchanged_coin_fee_amount'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS exchanged_coin_fee_amount,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'order_price'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS order_price,
+        JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'swap_price'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') AS swap_price,
+        if(JSONExtractString(arrayFilter(x -> (JSONExtractString(x,
+ 'key') = 'success'),
+ JSONExtractArrayRaw(JSONExtractString(event,
+ 'attributes')))[1],
+ 'value') = 'success',
+ true,
+ false) AS success
+    FROM
+    (
+        SELECT *
+        FROM spacebox.raw_block_results
+        WHERE end_block_events ILIKE '%swap_transacted%' and height >= 15515000
     )
     WHERE JSONExtractString(event,
  'type') = 'swap_transacted'
